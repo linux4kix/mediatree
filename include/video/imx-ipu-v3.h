9ed1fa69cdc885fd55ed765f3de70e99fd5ef9c0
Steve Longerbeam <steve_longerbeam@mentor.com>
2014-05-02 10:12:39 -0700

    gpu: ipu-v3: Add rotation mode conversion utilities
    Add two functions:
    
    - ipu_degrees_to_rot_mode(): converts a degrees, hflip, and vflip setting
      to an IPU rotation mode.
    - ipu_rot_mode_to_degrees(): converts an IPU rotation mode with given hflip
      and vflip settings to degrees.
    
    Signed-off-by: Steve Longerbeam <steve_longerbeam@mentor.com>

--- include/video/imx-ipu-v3.h
+++ include/video/imx-ipu-v3.h
@@ -77,6 +77,18 @@ enum ipu_color_space {
 	IPUV3_COLORSPACE_UNKNOWN,
 };
 
+enum ipu_rotate_mode {
+	/* Note the enum values correspond to BAM value */
+	IPU_ROTATE_NONE = 0,
+	IPU_ROTATE_VERT_FLIP = 1,
+	IPU_ROTATE_HORIZ_FLIP = 2,
+	IPU_ROTATE_180 = 3,
+	IPU_ROTATE_90_RIGHT = 4,
+	IPU_ROTATE_90_RIGHT_VFLIP = 5,
+	IPU_ROTATE_90_RIGHT_HFLIP = 6,
+	IPU_ROTATE_90_LEFT = 7,
+};
+
 struct ipuv3_channel;
 
 enum ipu_channel_irq {
@@ -367,6 +379,10 @@ int ipu_cpmem_set_image(struct ipu_ch_param __iomem *cpmem,
 enum ipu_color_space ipu_drm_fourcc_to_colorspace(u32 drm_fourcc);
 enum ipu_color_space ipu_pixelformat_to_colorspace(u32 pixelformat);
 enum ipu_color_space ipu_mbus_code_to_colorspace(u32 mbus_code);
+int ipu_degrees_to_rot_mode(enum ipu_rotate_mode *mode, int degrees,
+			    bool hflip, bool vflip);
+int ipu_rot_mode_to_degrees(int *degrees, enum ipu_rotate_mode mode,
+			    bool hflip, bool vflip);
 
 static inline void ipu_cpmem_set_burstsize(struct ipu_ch_param __iomem *p,
 		int burstsize)
