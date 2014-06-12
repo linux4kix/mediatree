95c0f08fd40586ebb885c5d04eb8b36328b5b8ef
Sascha Hauer <s.hauer@pengutronix.de>
2014-06-12 19:06:25 +0200

    v4l2: subdev: Add v4l2_device_register_subdev_node function
    We currently only have a function that registers all subdev
    device nodes for a v4l2 device at once. This assumes that
    there is a point when all subdevices are known and that all
    subdevices are needed to make a functional device. With the
    advent of asynchronous subdevices this may no longer be the
    case, so add a function which registers a single subdevice for
    a given v4l2 device only and let v4l2_device_register_subdev_nodes
    use this function.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- include/media/v4l2-device.h
+++ include/media/v4l2-device.h
@@ -120,6 +120,11 @@ void v4l2_device_unregister_subdev(struct v4l2_subdev *sd);
 int __must_check
 v4l2_device_register_subdev_nodes(struct v4l2_device *v4l2_dev);
 
+/* Register a single device node for a subdev of a v4l2 device. */
+int __must_check
+v4l2_device_register_subdev_node(struct v4l2_device *v4l2_dev,
+				 struct v4l2_subdev *sd);
+
 /* Send a notification to v4l2_device. */
 static inline void v4l2_subdev_notify(struct v4l2_subdev *sd,
 				      unsigned int notification, void *arg)
