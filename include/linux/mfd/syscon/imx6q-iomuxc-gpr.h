cca7b8c8c96938cb94c22bd23bb8b431a90889c7
Steve Longerbeam <steve_longerbeam@mentor.com>
2014-08-29 11:14:11 -0700

    ARM: imx6dl: Add iomux field defines for imx6dl
    Start adding iomux field defines for imx6dl. Adds GPR13 fields for
    MIPI-CSI2/Parallel video mux selection.
    
    Signed-off-by: Steve Longerbeam <steve_longerbeam@mentor.com>

--- include/linux/mfd/syscon/imx6q-iomuxc-gpr.h
+++ include/linux/mfd/syscon/imx6q-iomuxc-gpr.h
@@ -395,4 +395,11 @@
 #define IMX6SL_GPR1_FEC_CLOCK_MUX1_SEL_MASK    (0x3 << 17)
 #define IMX6SL_GPR1_FEC_CLOCK_MUX2_SEL_MASK    (0x1 << 14)
 
+/* iomux gpr register field defines for imx6dl  */
+#define IMX6DL_GPR13_MIPI_IPU_CSI1_MUX_SHIFT   3
+#define IMX6DL_GPR13_MIPI_IPU_CSI1_MUX_MASK    (0x7 << 3)
+#define IMX6DL_GPR13_MIPI_IPU_CSI0_MUX_SHIFT   0
+#define IMX6DL_GPR13_MIPI_IPU_CSI0_MUX_MASK    (0x7 << 0)
+
+
 #endif /* __LINUX_IMX6Q_IOMUXC_GPR_H */
